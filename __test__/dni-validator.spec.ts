f5fbd92eb56fce7505ca1756024339bae0fa1608
Rosana Ruiz <rosanarufer@gmail.com>
2018-08-31 13:10:28 +0100

    Refactor all tests

--- __test__/dni-validator.spec.ts
+++ __test__/dni-validator.spec.ts
@@ -1,27 +1,15 @@
 import { DNIValidator } from "../src/dni-validator";
 
 describe('DNI validator tests', () => {
-
+    function assertValidity(dni: string, expectedIsValid: boolean){
+        expect(DNIValidator.validate( dni)).toBe(expectedIsValid);
+    }
     it('A correct DNI is valid', () => {
-        // Arrange
-        const validDNI = '44650684W';
-
-        // Act
-        let isValid = DNIValidator.validate(validDNI);
-
-        // Assert
-        expect(isValid).toBe(true);
+        assertValidity('44650684W', true);
     });
 
     it('A DNI with less than 9 characters is not valid', () => {
-        // Arrange
-        const invalidDNI = '111W';
-
-        // Act
-        let isValid = DNIValidator.validate(invalidDNI);
-
-        // Assert
-        expect(isValid).toBe(false);
+        assertValidity('111W', false);
     });
 
 });
